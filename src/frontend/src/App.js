163d6103151dc5360ac6cd05497843e8af4b0fa3
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2021-04-12 15:47:19 +0200

    Update App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,15 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hello world 
-       
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      message : "Hello world"
+    }
+    this.clickMe = this.clickMe.bind(this);
+  }
+
+  clickMe(message){
+    this.setState({
+      message : message
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.message}
+        <button onClick={() => this.clickMe("Arvid")}>Arvid</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
