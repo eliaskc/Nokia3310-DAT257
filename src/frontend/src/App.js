2c220ef1734e256b59d1092375dcb1832360980c
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2021-04-15 11:34:04 +0200

    Update App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -24,13 +24,15 @@ class App extends Component {
         <header className="App-header">
         <img src="/hamncafeet-logo.jpeg" alt="Hamncafeet logga" />
         {this.state.message}
-        <Button onClick={() => this.clickMe("Hello world: 2")}>Arvid</Button>
-        <Button onClick={() => this.clickMe("Hello world: 3")}>Lisa</Button>
-        <Button onClick={() => this.clickMe("Hello world: 4")}>Elias H</Button>
-        <Button onClick={() => this.clickMe("Hello world: 5")}>Elias KC</Button>
-        <Button onClick={() => this.clickMe("Hello world: 6")}>Astrid</Button>
-        <Button onClick={() => this.clickMe("waddap")}>Erik</Button>
-        <Button onClick={() => this.clickMe("Hello world: 7")}>Ida :)</Button>
+        <div>
+          <Button onClick={() => this.clickMe("Hello world: 0")}>Arvid</Button>
+          <Button onClick={() => this.clickMe("Hello world: 1")}>Lisa</Button>
+          <Button onClick={() => this.clickMe("Hello world: 2")}>Elias H</Button>
+          <Button onClick={() => this.clickMe("Hello world: 3")}>Elias KC</Button>
+          <Button onClick={() => this.clickMe("Hello world: 4")}>Astrid</Button>
+          <Button onClick={() => this.clickMe("waddap")}>Erik</Button>
+          <Button onClick={() => this.clickMe("Hello world: 5")}>Ida :)</Button>
+        </div>
         </header>
       </div>
     );
