a2140d0d16311b371597bd9d4132c73303cc2424
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2021-04-15 10:47:21 +0200

    bootstrap buttons

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -24,11 +24,10 @@ class App extends Component {
         <header className="App-header">
         <img src="/hamncafeet-logo.jpeg" alt="Hamncafeet logga" />
         {this.state.message}
-        <button onClick={() => this.clickMe("Hello world: 1")}>namn1</button>
-        <button onClick={() => this.clickMe("Hello world: 2")}>namn2</button>
-        <button onClick={() => this.clickMe("Hello world: 3")}>Lisa</button>
-        <button onClick={() => this.clickMe("Hello world: 4")}>Elias H</button>
-        <button onClick={() => this.clickMe("Hello world: 5")}>Elias KC</button>
+        <Button onClick={() => this.clickMe("Hello world: 2")}>Arvid</Button>
+        <Button onClick={() => this.clickMe("Hello world: 3")}>Lisa</Button>
+        <Button onClick={() => this.clickMe("Hello world: 4")}>Elias H</Button>
+        <Button onClick={() => this.clickMe("Hello world: 5")}>Elias KC</Button>
         </header>
       </div>
     );
