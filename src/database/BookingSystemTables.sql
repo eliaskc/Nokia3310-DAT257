279d2998d8528f5fc409f8828cc4b27568a47391
idalinnakallio <81356924+idalinnakallio@users.noreply.github.com>
2021-04-21 11:53:07 +0200

    created tables
    Co-Authored-By: astridolaus <81357686+astridolaus@users.noreply.github.com>
    Co-Authored-By: lisamattus <81410674+lisamattus@users.noreply.github.com>

---
+++ src/database/BookingSystemTables.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE hamncafet_bookings;
+
+CREATE TABLE Tables (
+    tableID INT NOT NULL,
+    nrOfSeats INT = 2,
+    PRIMARY KEY(tableID),
+    CHECK nrOfSeats=2
+);
+
+CREATE TABLE GuestParties (
+    email TEXT PRIMARY KEY,
+    telNr TEXT NOT NULL,
+    nrOfPeople INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    UNIQUE(telNr),
+    CHECK (nrOfPeople <= 8 AND nrOfPeople >0)
+);
+
+CREATE TABLE Bookings (
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    bookingID TEXT NOT NULL,
+    guestP TEXT REFERENCES GuestParties(email),
+    PRIMARY KEY(date, time, guestP),
+    UNIQUE(bookingID)
+);
+
+CREATE TABLE BookedTables (
+    table INT REFERENCES Tables(tableID),
+    bookingDate DATE ,
+    bookingTime TIME ,
+    guestP TEXT ,
+    FOREIGN KEY(bookingDate, bookingTime, guestP) 
+        REFERENCES Bookings(date, time, guestP),
+    UNIQUE(bookingDate, bookingTime, guestP)
+);
