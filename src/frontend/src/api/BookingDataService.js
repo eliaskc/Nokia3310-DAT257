533da7ff3c1cea589a932419712d9d03ee10adc6
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2021-04-21 13:17:37 +0200

    added more calls to dataservice

--- src/frontend/src/api/BookingDataService.js
+++ src/frontend/src/api/BookingDataService.js
@@ -8,6 +8,28 @@ class BookingDataService {
     retrieveAllBookings() {
         return axios.get(`http://localhost:8080/bookings`);
     }
+
+    /**
+     * valid id's are positive non zero integers
+     * @returns A JSON object of a Booking
+     */
+    retrieveBooking(id) {
+        return axios.get(`http://localhost:8080/bookings/${id}`);
+    }
+
+    updateBooking(id,booking) {
+        return axios.put(`http://localhost:8080/bookings/${id}`, todo);
+    }
+
+    createBooking(booking) {
+        return axios.post(`http://localhost:8080/bookings`, booking);
+    }
+
+    deleteBooking(id) {
+        return axios.delete(`http://localhost:8080/bookings/${id}`);
+    }
+
+
 }
 
 export default new BookingDataService();
