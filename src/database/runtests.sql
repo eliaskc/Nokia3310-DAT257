ebec385786e77fa24fc6c7f3c80db5343571dd82
idalinnakallio <81356924+idalinnakallio@users.noreply.github.com>
2021-04-21 12:10:20 +0200

    created runtests file
    Co-Authored-By: astridolaus <81357686+astridolaus@users.noreply.github.com>
    Co-Authored-By: lisamattus <81410674+lisamattus@users.noreply.github.com>

---
+++ src/database/runtests.sql
@@ -0,0 +1,9 @@
+
+\set QUIET true
+SET client_min_messages TO WARNING; -- Less talk please.
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+\set QUIET false
+
+ \i /users/idalinnakallio/documents/GitHub/Nokia3310-DAT257/src/database/BookingSystemTables.sql
