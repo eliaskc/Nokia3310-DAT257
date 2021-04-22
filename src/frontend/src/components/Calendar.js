541a5f01bc510fe64d1e705866f6c7d0079b1ba0
Erik Wessman <erikwessman10@gmail.com>
2021-04-22 15:49:25 +0200

    Minor changes
    Disabled all dates except fridays and saturdays
    Added print statements for each option
    Fixed time list
    
    Co-Authored-By: Elias Hallberg <54537680+Elias-Hallberg@users.noreply.github.com>

--- src/frontend/src/components/Calendar.js
+++ src/frontend/src/components/Calendar.js
@@ -8,13 +8,25 @@ export default function calendar(props) {
 
     function tileDisabled({date}) {
         date.setHours(0,0,0,0)
+
+        //If it is not friday or saturday, disable date
+        if ((date.getDay() !== 5) && (date.getDay() !== 6)){
+            return true
+        }
+
+        //If the date is before today, disable date
         return (date < today);
+    }
+
+    function displayDate(date){
+        console.log(date)
+        props.dateProps(date)
     }
 
     return (
         <div className='calendar'>
             <h2><span>Pick a date</span></h2>
-            <Calendar tileDisabled={tileDisabled} onChange={(value) => props.dateProps(value)}></Calendar>
+            <Calendar tileDisabled={tileDisabled} onChange={(value) => displayDate(value)}></Calendar>
         </div>
     )
 }
--- src/frontend/src/components/Guests.js
+++ src/frontend/src/components/Guests.js
@@ -1,15 +1,20 @@
 import Button from 'react-bootstrap/Button';
 
 export default function Guests(props) {
+    function displayAmount(n){
+        console.log(n)
+        props.guestProps(n)
+    }
+
     return (
         <div>
             <h2><span>Number of guests</span></h2>
-            <Button onClick={() => props.guestProps(1)}>1</Button>
-            <Button onClick={() => props.guestProps(2)}>2</Button>
-            <Button onClick={() => props.guestProps(3)}>3</Button>
-            <Button onClick={() => props.guestProps(4)}>4</Button>
-            <Button onClick={() => props.guestProps(5)}>5</Button>
-            <Button onClick={() => props.guestProps(6)}>6</Button>
+            <Button onClick={() => displayAmount(1)}>1</Button>
+            <Button onClick={() => displayAmount(2)}>2</Button>
+            <Button onClick={() => displayAmount(3)}>3</Button>
+            <Button onClick={() => displayAmount(4)}>4</Button>
+            <Button onClick={() => displayAmount(5)}>5</Button>
+            <Button onClick={() => displayAmount(6)}>6</Button>
         </div>
     )
 }
