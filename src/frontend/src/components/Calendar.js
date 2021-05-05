cd1c72e50d5e516b6e443b79533b3121d0359b85
erikwessman <erikwessman10@gmail.com>
2021-05-05 14:37:03 +0200

    Attempt at disabling certain dates in calendar
    attempt failed :(
    
    Co-Authored-By: Elias Hallberg <54537680+Elias-Hallberg@users.noreply.github.com>

--- src/frontend/src/components/Calendar.js
+++ src/frontend/src/components/Calendar.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+
+import BookingDataService from '../api/BookingDataService'
 
 /**
  * Represents the calendar page
  * @param {*} props Object that represents the current booking
  * @returns 
  */
-export default function calendar(props) {
+export default function CalendarFunc(props) {
+    const [timelist, setTimeList] = useState()
+
     function tileDisabled({date}) {
         //Lägg till kod som kallar på backenden och returnerar 'true' om det inte finns någon ledig tid för dagen 'date'
         //Använd date.setHours(0,0,0,0)
         //Använd date.toLocaleDateString() för datumet
         //Använd date.toLocaleTimeString() för tiden
+
+        let dateTime = new Date()
+
+        if (date.toLocaleDateString() !== dateTime.toLocaleDateString()){
+            dateTime.setTime(0,0,0,0)
+        }
+        dateTime.toLocaleTimeString()
+
+        BookingDataService.retrieveAllAvailableTimes(date.toLocaleDateString(), new Date().toLocaleTimeString(), props.booking.guests)
+        .then(
+            (response) => {
+                setTimeList(reponse.data)
+            }
+        )
+
+        if (timelist.length === 0){
+            return true
+        } else {
+            return false
+        }
+
     }
 
     function displayDate(date){
