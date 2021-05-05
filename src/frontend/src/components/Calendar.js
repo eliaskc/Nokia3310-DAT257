3cafa441d715910848d3d026c8b574889f8b17b2
Erik Wessman <erikwessman10@gmail.com>
2021-05-05 21:06:03 +0200

    Days without available times are now grayed out automatically
    Uses react-spinners,
    do npm install
    
    Co-Authored-By: Elias Hallberg <54537680+Elias-Hallberg@users.noreply.github.com>
    Co-Authored-By: elias-carlson <23519471+elias-carlson@users.noreply.github.com>

--- src/frontend/src/components/Calendar.js
+++ src/frontend/src/components/Calendar.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import DotLoader from 'react-spinners/DotLoader'
+
+import BookingDataService from '../api/BookingDataService'
 
 /**
  * Represents the calendar page
  * @param {*} props Object that represents the current booking
  * @returns 
  */
-export default function calendar(props) {
-    let today = new Date()
-    today.setHours(0,0,0,0)
+export default function CalendarFunc(props) {
+    let loading = useRef(true)
+    const [reload, setReload] = useState(false)
+    const [dayList, setDayList] = useState([])
+
+    useEffect(() => {
+        BookingDataService.retrieveAllAvailableDays(props.booking.guests)
+            .then(
+                (response) => {
+                    setDayList(response.data)
+                    loading.current = false
+                    setReload(true)
+                }
+            )
+    }, [])
 
     function tileDisabled({date}) {
-        date.setHours(0,0,0,0)
-
-        //If the date is before today, disable date
-        return (date < today);
+        if (dayList.includes(date.toLocaleDateString())){
+            return false
+        } else {
+            return true
+        }
     }
 
     function displayDate(date){
@@ -25,8 +41,17 @@ export default function calendar(props) {
 
     return (
         <div className='calendar'>
-            <h2><span>Datum</span></h2>
-            <Calendar tileDisabled={tileDisabled} onChange={(value) => displayDate(value)}></Calendar>
+            <h2><span>Välj datum</span></h2>
+            {loading.current ? 
+            <DotLoader color={'#FFFFFF'}/>
+            :
+            <Calendar
+            tileDisabled={tileDisabled}
+            minDate={new Date()}
+            minDetail='month'
+            onChange={(value) => displayDate(value)}>
+            </Calendar>
+            }
         </div>
     )
 }
