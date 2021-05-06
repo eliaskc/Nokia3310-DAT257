50ad7ef1638ed871c62a8962bffb858d6ad86081
Elias Carlson <carlson.elias@gmail.com>
2021-05-06 10:48:21 +0200

    eairest

--- src/frontend/src/components/BookingOverview/BookingComponent.jsx
+++ src/frontend/src/components/BookingOverview/BookingComponent.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 
 export default function BookingComponent(props) {
     return (
-            
-                <tr>
-                    <td>{props.booking.guestName}</td>
-                    <td>{props.booking.guestEmail}</td>
-                    <td>{props.booking.nrOfPeople}</td>
-                </tr>    
+        <div></div>
     )
 }
--- src/frontend/src/components/BookingOverview/BookingTimeSlotComponent.jsx
+++ src/frontend/src/components/BookingOverview/BookingTimeSlotComponent.jsx
@@ -4,11 +4,14 @@ import BookingDataService from '../../api/BookingDataService.js'
 import BookingComponent from './BookingComponent.jsx';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal'
 
 function BookingTimeSlotComponent(props) {
     const prevProps = useRef();
     const [bookings, setBookings] = useState([]);
     const [isExpanded, setExpanded] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    //const [modalBooking, setModalBooking] = useState();
     const [numberOfBookings, setNumberOfBookings] = useState(refreshNumberOfGuests(props.inputDate, props.inputTime));
 
     useEffect(() => {
@@ -36,6 +39,12 @@ function BookingTimeSlotComponent(props) {
             setExpanded(true);
     }
 
+    const handleCloseModal = () => setShowModal(false)
+    const handleShowModal = (booking) => {
+        setShowModal(true);
+        //setModalBooking(booking);
+    }
+
     function refreshNumberOfGuests(date, time) {
         date = moment(date).format('YYYY-MM-DD')
         BookingDataService.getNumberOfBookingsByDateAndTime(date, time)
@@ -56,16 +65,34 @@ function BookingTimeSlotComponent(props) {
                     <tr>
                         <th>Namn:</th>
                         <th>Epost:</th>
-                        <th>Antal bord:</th>
+                        <th>Antal gäster:</th>
                     </tr>
                 </thead>
                 <tbody>
-                {bookings.map(
-                    booking =>
-                        <BookingComponent booking={booking} />
-                )}
+                    {bookings.map(
+                        booking =>
+                            <tr className="booking" onClick={handleShowModal(booking)}>
+                                <td>{booking.guestName}</td>
+                                <td>{booking.guestEmail}</td>
+                                <td>{booking.nrOfPeople}</td>
+                            </tr>
+                    )}
                 </tbody>
             </Table>
+            {/*<Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>{modalBooking.guestName}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseModal}>
+                    Close
+            </Button>
+                <Button variant="primary" onClick={handleCloseModal}>
+                    Save Changes
+            </Button>
+            </Modal.Footer>
+                </Modal>*/}
         </tr>
     )
 
