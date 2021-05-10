f5cd5e3a17d79c6155f3ac6d8e56a97195260043
Elias Carlson <carlson.elias@gmail.com>
2021-05-10 10:50:40 +0200

    fixed merge issues

--- src/frontend/src/components/HomeComponent.jsx
+++ src/frontend/src/components/HomeComponent.jsx
@@ -3,12 +3,12 @@ import React, { useState, useRef } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom'
 import DownloadInstructions from './DownloadInstructions';
 import Wizard from './Wizard'
-import Contact from './Contact'
 
 /**
  * Component for the Home page 
  */
 function HomeComponent() {
+    const [show, setShow] = useState(false)
     return (
         <div className="App">
             <div className='background-image'>
@@ -22,32 +22,28 @@ function HomeComponent() {
                         <Route exact path='/'>
                             <Button href="/guests">Boka bord</Button>
                             <Button href="/bookings">Se bokningar</Button>
-                            <Button className='contact-btn' href="/contact" >Kontakt</Button>
+                            <div>
+                                <Button className='contact-btn' onClick={() => setShow(!show)}>Kontakt</Button>
+
+                                {
+                                    show ? <div class="card" className="contact-card">
+                                        <div class="card-body">
+                                            <div className="italictext">
+                                                <em>Har ni några frågor eller vill hellre boka via telefon eller mail? Tveka inte att kontakta oss!<br />
+                                                </em></div>
+                                            <div>
+                                                Telefon: <a href="tel:0304-570-07">0304-570 07</a> <br />
+                                                Email: <a href="mailto:info@gullholmenshamncafe.se">info@gullholmenshamncafe.se</a></div>
+                                        </div>
+                                    </div> : null
+                                }
+                            </div>
                         </Route>
                         <Route path='/guests'>
                             <Wizard />
                         </Route>
-                        <Route path='/contact'>
-                            <Contact />
-                        </Route>
                     </Switch>
                 </Router>
-                <div>
-                    <Button className='contact-btn' onClick={() => setShow(!show)}>Kontakt</Button>
-
-                    {
-                        show ? <div class="card" className="contact-card">
-                            <div class="card-body">
-                                <div className="italictext">
-                                    <em>Har ni några frågor eller vill hellre boka via telefon eller mail? Tveka inte att kontakta oss!<br />
-                                    </em></div>
-                                <div>
-                                    Telefon: <a href="tel:0304-570-07">0304-570 07</a> <br />
-                                    Email: <a href="mailto:info@gullholmenshamncafe.se">info@gullholmenshamncafe.se</a></div>
-                            </div>
-                        </div> : null
-                    }
-                </div>
                 <DownloadInstructions />
             </header>
         </div>
