f1ccdf1470eda19a173963e7ffb2b51380433fbb
ArvidHolmqvist <arvidholmqvist98@gmail.com>
2021-05-14 10:00:17 +0200

    added jwt classes

---
+++ src/backend/booking/src/main/java/se/chalmers/TDA257/booking/JWT/JwtAuthenticationController.java
@@ -0,0 +1,61 @@
+package se.chalmers.TDA257.booking.JWT;
+
+import java.util.Objects;
+
+import se.chalmers.TDA257.booking.JWT.JwtUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+import se.chalmers.TDA257.booking.JWT.JwtTokenUtil;
+import se.chalmers.TDA257.booking.JWT.JwtRequest;
+import se.chalmers.TDA257.booking.JWT.JwtResponse;
+
+@RestController
+@CrossOrigin
+public class JwtAuthenticationController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+
+	@Autowired
+	private JwtUserDetailsService jwtInMemoryUserDetailsService;
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
+			throws Exception {
+
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+		final UserDetails userDetails = jwtInMemoryUserDetailsService
+				.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		Objects.requireNonNull(username);
+		Objects.requireNonNull(password);
+
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+}
